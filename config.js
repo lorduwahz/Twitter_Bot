d532adeab5a48cfe663047743babd378070fe93d
Francis Euston <lorduwahz@users.noreply.github.com>
2017-06-15 23:50:24 +0100

    Create config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: '1h8MrL2Q2gHATlNYNZoE7iNgN',
-    consumer_secret: 'I3ehyzq2fos2FdBxjTN7HyaxwUcwQLl6k1M8EeZ6eYOk5a2LVy',
-    access_token_key: '926078672-gMU2AufzHkxHip7Mnnju0XQikPWuZkGfCQJTCY97',
-    access_token_secret: 'wyBmqGXvJmv7XeZOXTwZHbY0IewAXVZXPWDUBOBFzzdxC'
+    consumer_key: v1'1h8MrL2Q2gHATlNYNZoE7iNgN',
+    consumer_secret: v2'I3ehyzq2fos2FdBxjTN7HyaxwUcwQLl6k1M8EeZ6eYOk5a2LVy',
+    access_token_key: v3'926078672-gMU2AufzHkxHip7Mnnju0XQikPWuZkGfCQJTCY97',
+    access_token_secret:v4 'wyBmqGXvJmv7XeZOXTwZHbY0IewAXVZXPWDUBOBFzzdxC'
 }
