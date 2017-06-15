5a5beb8be8084405f8236d27a271d2bb6c28b67b
Francis Euston <lorduwahz@users.noreply.github.com>
2017-06-16 00:01:17 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: v1'1h8MrL2Q2gHATlNYNZoE7iNgN',
-    consumer_secret: v2'I3ehyzq2fos2FdBxjTN7HyaxwUcwQLl6k1M8EeZ6eYOk5a2LVy',
-    access_token_key: v3'926078672-gMU2AufzHkxHip7Mnnju0XQikPWuZkGfCQJTCY97',
-    access_token_secret:v4 'wyBmqGXvJmv7XeZOXTwZHbY0IewAXVZXPWDUBOBFzzdxC'
-}
