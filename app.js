f8b39d10bfe9a00f2d11fb6fb885855abbc14295
Francis Euston <lorduwahz@users.noreply.github.com>
2017-06-15 23:38:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+//....setting up requirements............
+
+var Twitter = require('twitter');
+var config = require('./config.js');
+
+//....Our main objecj..............
+
+var T = new Twitter(config);
+
+//...setting out our parameters............
+
+var params = {
+    q: '#nodejs',
+    count: 10,
+    result_type: 'recent',
+    lang: 'en'
+}
+
+//............Get/Post request............................
+
+T.get('search/tweets', params, function(err, data, response) {
+
+    if (!err) {
+        for (var i = 0; i < data.statuses.length; i++) {
+            let id = { id: data.statuses[i].id_str }
+            T.post('favorites/create', id, function(err, response) {
+                if (err) {
+                    console.log(err[0].message);
+                } else {
+                    let username = response.user.screen_name;
+                    let tweetId = response.id_str;
+                    console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+                }
+            })
+        }
+    } else {
+        console.log(err);
+    }
+})
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: '1h8MrL2Q2gHATlNYNZoE7iNgN',
+    consumer_secret: 'I3ehyzq2fos2FdBxjTN7HyaxwUcwQLl6k1M8EeZ6eYOk5a2LVy',
+    access_token_key: '926078672-gMU2AufzHkxHip7Mnnju0XQikPWuZkGfCQJTCY97',
+    access_token_secret: 'wyBmqGXvJmv7XeZOXTwZHbY0IewAXVZXPWDUBOBFzzdxC'
+}
